4b07544ce554eb706b9cd184f611310667215068
rinocerosa <anaischaouati@gmail.com>
2024-04-12 15:09:12 +0200

    uu

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("https://whatyearisit-backend.vercel.app/year")
     .then((response) => response.json())
     .then((data) => {
-      console.log("Frontend: Received response from backend:", data);
       document.getElementById("year").textContent = data.year;
     })
     .catch((error) =>
-      console.error("Frontend: Error fetching current year:", error)
+      console.error("Erreur lors de la récupération de l'année:", error)
     );
 });
